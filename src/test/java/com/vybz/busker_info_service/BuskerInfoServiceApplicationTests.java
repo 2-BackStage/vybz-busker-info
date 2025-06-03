3ad678dc9ff91c70113a7b525abdf5da8b1a0733
HyeonProG <guswo3443@naver.com>
2025-06-03 12:32:32 +0900

    first commit

---
+++ src/test/java/com/vybz/busker_info_service/BuskerInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vybz.busker_info_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuskerInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
