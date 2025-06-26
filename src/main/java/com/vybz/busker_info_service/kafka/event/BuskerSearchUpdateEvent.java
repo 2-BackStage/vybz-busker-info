84633cb95344bd270a6c2f2b60da6c57d7d8290f
jin-sung-oh <wlsqoren@naver.com>
2025-06-26 10:48:10 +0900

    feat:초성util추가 (#23)

--- src/main/java/com/vybz/busker_info_service/kafka/event/BuskerSearchUpdateEvent.java
+++ src/main/java/com/vybz/busker_info_service/kafka/event/BuskerSearchUpdateEvent.java
@@ -10,14 +10,17 @@ public class BuskerSearchUpdateEvent {
 
     private String buskerUuid;
     private String nickname;
+    private String nicknameChosung;
     private String profileImageUrl;
 
     @Builder
     public BuskerSearchUpdateEvent(String buskerUuid,
                                    String nickname,
+                                   String nicknameChosung,
                                    String profileImageUrl) {
         this.buskerUuid = buskerUuid;
         this.nickname = nickname;
+        this.nicknameChosung = nicknameChosung;
         this.profileImageUrl = profileImageUrl;
     }
 }
